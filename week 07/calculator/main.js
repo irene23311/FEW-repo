46054dee8a996909c42c5dc03709ed49de47151f
igwe-bee <sl11551@nyu.edu>
2025-10-16 21:12:38 -0400

    basic

--- week 07/calculator/main.js
+++ week 07/calculator/main.js
@@ -1,20 +1,16 @@
 function computeAndDisplay() {
-    let a= parseInt(value1.value);
-    let b= parseInt(value2.value);
+    let a = parseInt(value1.value);
+    let b = parseInt(value2.value);
 
     console.log("Value of a: ", a);
 
-    if(a==0){
-    alert("Value 1 cannot be zero");
-    }  
-     elseif (isNaN(a))
-    {
+    if (a == 0) {
+        alert("Value 1 cannot be zero");
+    } else if (isNaN(a)) {
         alert("Please enter a valid number");
     }
-
-    
-    let result= a + b;
-    result.innerHTML= "Result: " + result;
+    let c = a + b;
+    result.innerText = c;
 }
 
-computeButton.onclick= computeAndDisplay;
+computeButton.onclick = computeAndDisplay;
